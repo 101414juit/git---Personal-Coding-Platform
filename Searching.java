d6b299974f1dd14530a5f57b933e301076ff3cd5
Aman <rastogiaman690@gmail.com>
2018-06-13 13:51:29 +0530

    Update Searching.java
    Updated Binary search.

--- Searching.java
+++ Searching.java
@@ -11,24 +11,28 @@ import java.util.Scanner;
 public class Searching {
 	public static void main(String[] args){
 		Scanner sc = new Scanner(System.in);
-		int[] input = {2, 16, 49, 62, 71, 74, 86, 100, 110, 111, 116, 127, 128, 129, 138, 140, 148, 161, 167, 174, 177, 179, 180, 184, 215, 220, 229, 230, 236, 241, 252, 270, 303, 314, 322, 328, 331};
+		//int[] input = {2, 16, 49, 62, 71, 74, 86, 100, 110, 111, 116, 127, 128, 129, 138, 140, 148, 161, 167, 174, 177, 179, 180, 184, 215, 220, 229, 230, 236, 241, 252, 270, 303, 314, 322, 328, 331};
+		int[] input = {5,4,3,2,1};
 		int option;
-		System.out.println("Insert integer key to search");
-		int key = sc.nextInt();
+//		System.out.println("Insert integer key to search");
+//		int key = sc.nextInt();
 		
 		while(true){
 
-
+			System.out.println("Insert integer key to search");
+			int key = sc.nextInt();
 			System.out.println("Please select options below: ");
 			System.out.println();
-			System.out.println("********1. LinearSearch\t 2. Binary Search\t 3. Jump Search *********");
+			System.out.println("********\t1. LinearSearch\t 2. Binary Search\t 3. Jump Search\t*********");
 			option = sc.nextInt();
 			switch (option){
 			case 1: if(linearSearch(input,key)==0)
 				System.out.println("Key doesn't exists!!");
 			break;
-			case 2: if(binarySearch(input,0, input.length-1,key)==0)
-				System.out.println("Value not found!!");
+			case 2: sortInIncreasingOrderUsingBubbleSort(input); 
+				if(binarySearch(input,0, input.length-1,key)==1)
+					System.out.println("Value found!!");
+				else System.out.println("Sorry!!");
 			break;
 			case 3: if(jumpSearch(input,key)==0)
 				System.out.println("Key not found");
@@ -54,14 +58,15 @@ public class Searching {
 	 * Sorting an array in Increasing order using Bubble sort
 	 */
 	public static int binarySearch(int[] input,int start, int last, int key){
-		sortInIncreasingOrderUsingBubbleSort(input);
-		int  mid=last/2;
-		if( key == input[mid])
-			return 1;
-		else if(key > input[mid])
-			binarySearch(input, mid, last, key);
-		else if(key< input[mid])
-			binarySearch(input, start, mid, key);
+		if(last>=start){
+			int  mid=(start+last)/2;
+			if( key == input[mid])
+				return 1;
+			else if(key > input[mid])
+				return binarySearch(input, mid+1, last, key);
+			else if(key< input[mid])
+				return binarySearch(input, start, mid-1, key);
+		}
 		return 0;
 	}
 	
